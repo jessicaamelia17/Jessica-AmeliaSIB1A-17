4a810854c6289e668ce28d78e0c84d10357157cc
jessicaamelia17 <144074335+jessicaamelia17@users.noreply.github.com>
2023-10-10 12:32:02 +0700

    Jobsheet7

---
+++ Jobsheet7/WhileKelipatan16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class WhileKelipatan16 {
+    public static void main(String[] args){
+       Scanner input16 = new Scanner(System.in);
+        //deklarasi 
+        int kelipatan, total=0, counter=0;
+        //output 
+        System.out.print("Masukkan bilangan kelipatan (1-9):");
+        kelipatan= input16.nextInt();
+        int i=1;
+        while (i<=50){
+            if (i%kelipatan==0){
+                total += i;
+                counter++;   
+        } i++;
+    }   
+    double rataRata = (double) total / counter;
+    System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+    System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+    System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+}
+}
